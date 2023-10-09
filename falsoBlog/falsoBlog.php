da7c12a400ebff5bb28e3db19562c7aeff80efdd
Cristopher Escalante <cristopheralex2001@hotmail.com>
2023-10-09 14:17:19 +0200

    blog

--- falsoBlog/falsoBlog.php
+++ falsoBlog/falsoBlog.php
@@ -1,33 +1,37 @@
-<head>
-    <title>Blog de Cris</title>
-</head>
-
-<body>
 <?php
-$mensaje = "Texto Vacío";
- if(! isset($_REQUEST["enviar"])){  
-    if($mensaje == "Texto Vacio"){
-        $mensaje=$_REQUEST["texto"];
-    }
+// Inicializar la variable de texto
+$textoCompleto = '';
+
+// Verificar si se envió el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nuevoTexto = $_POST["texto"];
+    
+    // Obtener el texto existente del campo oculto (si existe)
+    $textoExistente = isset($_POST["texto_completo"]) ? $_POST["texto_completo"] : '';
+    
+    // Concatenar el nuevo texto con el texto existente
+    $textoCompleto = $textoExistente . "\n" . $nuevoTexto;
+}
 ?>
-        <form action="falsoBlog.php" method="post">
-            <label type="" name="blog"><?php echo $mensaje?></input><br>
-            <input type="text" name="texto"/>   
-            <input type="submit" name="enviar" value="Enviar"/>
-        </form>
-        <?php 
-        }else{
-        ?>
-        <form action="falsoBlog.php" method="post">
-            <label type="" name="blog"><?php echo $mensaje?></input><br>
-            <input type="text" name="texto"/>   
-            <input type="submit" name="enviar" value="Enviar"/>
-        </form>
-        <?
-        }
-    ?>
-        
-</body>
-</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ejemplo PHP</title>
+</head>
+<body>
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <label for="texto">Ingrese un texto:</label>
+        <input type="text" name="texto" id="texto">
+        <input type="hidden" name="texto_completo" value="<?php echo $textoCompleto; ?>">
+        <input type="submit" value="Enviar">
+    </form>
+
+    <hr>
+    
+    <label for="resultado">Texto ingresado:</label>
+    <textarea width="10000px" name="resultado" id="resultado" readonly>
+        <?php echo $textoCompleto; ?>
+    </textarea>
 </body>
 </html>
